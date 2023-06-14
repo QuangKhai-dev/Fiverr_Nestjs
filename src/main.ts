a3b940a40b567e6f5308c4006b770651fbeb4896
Đỗ Quang Khải <88335848+QuangKhai-dev@users.noreply.github.com>
2023-06-14 21:29:33 +0700

    update port env

--- src/main.ts
+++ src/main.ts
@@ -62,6 +62,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/swagger', app, document);
 
-  await app.listen(process.env.NODE_LOCAL_PORT || 3001);
+  await app.listen(process.env.PORT || 3001);
 }
 bootstrap();
